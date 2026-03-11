1ac81e5e5856594a51e11470bb63320bb7e5f086
Viciclentiu <126006135+Viciclentiu@users.noreply.github.com>
2026-03-11 09:19:18 +0200

    Slight modifications for the class

--- main.cpp
+++ main.cpp
@@ -8,34 +8,37 @@ private:
     char *name;
     double quantity;
 public:
-    Ingredient(): id(++no_ingredients) {
-        name = new char[1];
-        name[0] = '\0';
-        quantity = 0.0;
-    }
-    Ingredient(const char* n, double q): id(++no_ingredients), quantity(q) {
-        this->name = new char[strlen(n)+1];
-        strcpy(this->name, n);
-    };
-    ~Ingredient() {
-        delete[] name;
-    }
-    Ingredient(const Ingredient &obj) : id(++no_ingredients) {
-        this->name = strcpy(new char[strlen(obj.name)+1],obj.name);
-        this->quantity = obj.quantity;
-    };
-    Ingredient& operator=(const Ingredient &obj) {
-        if (this == &obj) {
-            return *this;
-        }
-        delete[] this->name;
-        this->name = strcpy(new char[strlen(obj.name)+1],obj.name);
-        this->quantity = obj.quantity;
+    Ingredient();
+    Ingredient(const char* n, double q);
+    ~Ingredient();
+    Ingredient(const Ingredient &obj);
+    Ingredient& operator=(const Ingredient &obj);
+};
+Ingredient::Ingredient(): id(++no_ingredients) {
+    name = new char[1];
+    name[0] = '\0';
+    quantity = 0.0;
+}
+Ingredient::Ingredient(const char* n, double q): id(++no_ingredients), quantity(q) {
+    this->name = new char[strlen(n)+1];
+    strcpy(this->name, n);
+};
+Ingredient::~Ingredient() {
+    delete[] name;
+}
+Ingredient::Ingredient(const Ingredient &obj) : id(++no_ingredients) {
+    this->name = strcpy(new char[strlen(obj.name)+1],obj.name);
+    this->quantity = obj.quantity;
+};
+Ingredient& Ingredient::operator=(const Ingredient &obj) {
+    if (this == &obj) {
         return *this;
     }
-};
-
-
+    delete[] this->name;
+    this->name = strcpy(new char[strlen(obj.name)+1],obj.name);
+    this->quantity = obj.quantity;
+    return *this;
+}
 
 int main()
 {
