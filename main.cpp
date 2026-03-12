928973db91b5f3962d3081be5985a25de434c7af
Viciclentiu <126006135+Viciclentiu@users.noreply.github.com>
2026-03-13 00:56:19 +0200

    Added the recipe and fridge classes

--- main.cpp
+++ main.cpp
@@ -40,6 +40,111 @@ Ingredient& Ingredient::operator=(const Ingredient &obj) {
     return *this;
 }
 
+
+
+class Recipe{
+private:
+    Ingredient* ingredients;
+    char* instructions;
+    int count;
+    bool can_make;
+public:
+    Recipe();
+    Recipe(const char*,const Ingredient*,int,bool);
+    ~Recipe();
+    Recipe(const Recipe &obj);
+    Recipe& operator=(const Recipe &obj);
+};
+Recipe::Recipe() {
+    ingredients = nullptr;
+    instructions = new char[16];
+    strcpy(instructions, "No recipes");
+    count= 0;
+    can_make=false;
+}
+Recipe::Recipe(const char *ins, const Ingredient *ingred,int count,bool canmake) {
+        this->count= count;
+        this->ingredients = new Ingredient[count];
+        this->ingredients[count] = *ingred;
+        this->instructions = new char[256];
+        strcpy(this->instructions,ins);
+        this->can_make = canmake;
+}
+Recipe::~Recipe() {
+    delete[] ingredients;
+    delete[] instructions;
+}
+Recipe::Recipe(const Recipe &obj) {
+    this->count= obj.count;
+    this->ingredients = new Ingredient[obj.count];
+    this->ingredients[obj.count] = *obj.ingredients;
+    this->instructions = new char[256];
+    strcpy(this->instructions,obj.instructions);
+    this->can_make = obj.can_make;
+}
+Recipe& Recipe::operator=(const Recipe &obj) {
+    if (this == &obj) {
+        return *this;
+    }
+    delete[] ingredients;
+    this->ingredients = new Ingredient[obj.count];
+    this->ingredients[obj.count] = *obj.ingredients;
+    this->instructions = new char[256];
+    strcpy(this->instructions,obj.instructions);
+    this->can_make = obj.can_make;
+    return *this;
+}
+
+
+class Fridge {
+private:
+    float temp;
+    bool isOpen;
+    int capacity;
+    char *observations;
+public:
+    Fridge();
+    Fridge(const char*,float,int,bool);
+    ~Fridge();
+    Fridge(const Fridge &obj);
+    Fridge& operator=(const Fridge &obj);
+};
+Fridge::Fridge() {
+    temp = 0.0;
+    isOpen = false;
+    capacity = 0;
+    observations = new char[16];
+    strcpy(observations,"No observations");
+}
+Fridge::Fridge(const char* observations, float temp, int capacity,bool isOpen) {
+    this->capacity = capacity;
+    this->observations = new char[strlen(observations)];
+    strcpy(this->observations,observations);
+    this->isOpen = isOpen;
+    this->temp = temp;
+}
+Fridge::~Fridge() {
+    delete[] observations;
+}
+
+Fridge::Fridge(const Fridge &obj) {
+    this->capacity = obj.capacity;
+    this->observations = new char[strlen(obj.observations)];
+    strcpy(this->observations,obj.observations);
+    this->isOpen = obj.isOpen;
+    this->temp = obj.temp;
+}
+Fridge& Fridge::operator=(const Fridge &obj) {
+    if(this == &obj) {
+        return *this;
+    }
+    delete[] observations;
+    this->observations= new char[strlen(obj.observations)];
+    strcpy(this->observations,obj.observations);
+    this->isOpen = obj.isOpen;
+    this->temp = obj.temp;
+    return *this;
+}
 int main()
 {
 
